320c2be7d43909b14d82356114e2120bdf72f928
Alex <alexandersimcock@gmail.com>
2022-12-21 15:43:23 +0000

    Minor

--- mr_freeze/main.py
+++ mr_freeze/main.py
@@ -16,17 +16,24 @@ def saver(**kwargs) -> None:
 def virtual_env_detector_naive() -> Optional[str]:
     """Naively scans for lock files relevant to venv"""
     files = [file.name for file in os.scandir()]
+    # TODO: Work on how to determine if in Conda environment
     if "Pipfile" in files:
         return "pipenv"
     if "poetry.lock" in files:
         return "poetry"
 
 
-# def main(argv: Sequence[str] | None = None) -> None:
+def requirements_linter() -> None:
+    """Lints the requirements.txt file so pre-commit passes"""
+    print("running linter")
+    os.system("pre-commit run --file=requirements.txt")
+
+
 def main() -> None:
     """Initial Entry Point"""
     venv_method = virtual_env_detector_naive()
     saver(method=venv_method)
+    requirements_linter()
 
 
 if __name__ == "__main__":
